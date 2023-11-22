55d12a0478a1e229ee9a42f598758e9d679e32b1
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-11-22 12:34:25 -0800

    promised .all

--- Week8/Day3/01-create-and-handle-promises/phase-1.js
+++ Week8/Day3/01-create-and-handle-promises/phase-1.js
@@ -2,8 +2,8 @@ function stretch() {
   // Your code here
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log("done stretching");
-      resolve();
+      // console.log("done stretching");
+      resolve("done stretching");
     }, 1000);
   });
 }
@@ -11,9 +11,11 @@ function stretch() {
 function runOnTreadmill() {
   // Your code here
   return new Promise((resolve, reject) => {
+    // return reject(new Error("Error"));
     setTimeout(() => {
-      console.log("done running on treadmill");
-      resolve();
+      // reject("error");
+      // console.log("done running on treadmill");
+      resolve("done running on treadmill");
     }, 500);
   });
 }
@@ -22,19 +24,28 @@ function liftWeights() {
   // Your code here
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("done lifting weights");
-      resolve();
+      // console.log("done lifting weights");
+      resolve("done lifting weights");
     }, 2000);
   });
 }
 
-function workout() {
+async function workout() {
   //.then(() => nextFunc()) <-- can chain .thens
   // Your code here
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"));
+  // stretch()
+  //   .then(runOnTreadmill)
+  //   .then(liftWeights)
+  //   .then(() => console.log("done working out"));
+  // await stretch();
+  // await runOnTreadmill();
+  // await liftWeights();
+  // console.log("done working out");
+  return Promise.all([stretch(), runOnTreadmill(), liftWeights()])
+    .then((promise) => {
+      console.log("results", promise);
+    })
+    .catch((err) => console.log(err));
 }
 
 /* ============================ TEST YOUR CODE ============================
