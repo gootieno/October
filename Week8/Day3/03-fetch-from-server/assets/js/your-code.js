60be1c717320f5d02bf484a3fd149dd9c8fffdd0
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-11-22 13:13:34 -0800

    fetch

--- Week8/Day3/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/03-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,39 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+
+  const url = "/dogs";
+
+  // Use the URLSearchParams API to format your body as shown below
+
+  return fetch(url);
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
 }
 
 export function postNewDog() {
-    // Your code here
+  // Your code here
+  const url = "/endpoint";
+  const headers = { "Content-Type": "Request body's Content-Type" };
+  // Use the URLSearchParams API to format your body as shown below
+  const body = new URLSearchParams({
+    key: "value",
+  });
+
+  const options = {
+    method: "GET|POST|PUT|DELETE",
+    headers: headers,
+    body: body,
+  };
+
+  return fetch(url, options);
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  // Your code here
 }
 
 export function deleteDog(id) {
-      // Your code here
+  // Your code here
 }
