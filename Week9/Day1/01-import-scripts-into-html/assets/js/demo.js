2c1a4a840ddcd84030827d7e555ff0ad98ef6138
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-11-28 15:02:48 -0800

    scripts

---
+++ Week9/Day1/01-import-scripts-into-html/assets/js/demo.js
@@ -0,0 +1,11 @@
+// const initializePage = () => {
+// some code here
+// }
+
+// window.onload = initializePage
+// or
+window.onload = () => {
+  const pTag = document.body.children[1];
+
+  console.log("p tag  ", pTag);
+};
