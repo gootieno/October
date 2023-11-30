8f0ff6fde7c44f142927dd34007660215a471664
Tanner Shaw <traumaaa@gmail.com>
2023-11-30 13:19:26 -0600

    w9d3 solutions

--- Week9/Day3/02-store-data-on-elements/data.js
+++ Week9/Day3/02-store-data-on-elements/data.js
@@ -1,1 +1,23 @@
 // Your code here
+
+window.addEventListener('DOMContentLoaded', () => {
+  const shoppingList = document.getElementById('shopping-list');
+
+  const name = document.getElementById('name');
+  const type = document.getElementById('type');
+
+  const addBtn = document.getElementById('add');
+
+  addBtn.addEventListener('click', e => {
+    e.preventDefault();
+
+    const li = document.createElement('li');
+
+    li.innerText = name.value;
+    li.dataset.type = type.value;
+    li.setAttribute('data-type', type.value);
+
+    shoppingList.appendChild(li);
+  })
+
+})
